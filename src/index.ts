e0e1705d76818bb54e923080d4489c6627fdb735
Bruno Santos <brunoxdsantos.bs@gmail.com>
2023-06-29 11:14:23 -0300

    fix: removendo tentativa de rate limit

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,11 @@ import clansRouter from "./routes/clans-route";
 import favoriteClansRouter from "./routes/favorite-clans-route";
 import favoriteCharacterRouter from "./routes/favorite-character-route";
 import characterRouter from "./routes/character-route";
-import rateLimit from "express-rate-limit";
 
 const PORT = process.env.PORT || 4000;
 const HOSTNAME = process.env.HOSTNAME || "http://localhost";
 
 const app = express();
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, //(15 minutos)
-  max: 0, 
-  message: "Limite de requisições excedido. Tente novamente mais tarde.",
-});
-
-app.use(limiter);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
